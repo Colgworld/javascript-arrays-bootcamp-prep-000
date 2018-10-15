b43dda9b2f73ea5f0453ff8223a80d142fe3e9b7
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-15 22:29:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,62 +1,23 @@
-chocolateBars= [];
-
-function addElementToBeginningOfArray(chocolateBars, moreChocolateBars) {
-  var chocolateBars = [
-    'snickers',
-    'hundred grand',
-    'kitkat',
-    'skittles'
-  ]
-
-  var moreChocolateBars = 'reeses';
-  chocolateBars.unshift(moreChocolateBars);
-  return chocolateBars
-}
-
-
-
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars2, moreChocolateBars2) {
-  var chocolateBars2 = [
+var chocolateBars = [
     'snickers',
     'hundred grand',
     'kitkat',
     'skittles',
   ];
 
-  var moreChocolateBars2 = 'reeses';
-  addABar = chocolateBars2.unshift(moreChocolateBars2);
-  return addABar;
+
+function addElementToBeginningOfArray(, ) {
+
 }
 
+function destructivelyAddElementToBeginningOfArray(, ) {
 
-
-
-function addElementToEndOfArray(chocolateBars3, moreChocolateBars3) {
-  var chocolateBars3 = [
-    'snickers',
-    'hundred grand',
-    'kitkat',
-    'skittles'
-  ]
-
-  var moreChocolateBars3 = 'reeses';
-  chocolateBars3.push(moreChocolateBars3);
-  return chocolateBars3
 }
 
+function addElementToEndOfArray(, ) {
 
+}
 
+function destructivelyAddElementEndOfArray(, ) {
 
-function destructivelyAddElementEndOfArray(chocolateBars, moreChocolateBars) {
-  var chocolateBars = [
-    'snickers',
-    'hundred grand',
-    'kitkat',
-    'skittles'
-  ]
-
-  var moreChocolateBars = 'reeses';
-  chocolateBars.push(moreChocolateBars);
-  return chocolateBars
 }
