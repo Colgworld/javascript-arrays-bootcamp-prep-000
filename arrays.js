1d2e50f8a863ad1b3b4f86cf483e27f96e978a87
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-15 22:06:56 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(chocolateBars, moreChocolateBars) {
+  var moreChocolateBars = 'reeses'
+  chocolateBars.push(moreChocolateBars);
+  return chocolateBars
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementEndOfArray() {
+
+}
